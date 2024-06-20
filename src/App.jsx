aa7f96c827b2f028f1bf4032a48d3564af9c30b6
Brian Harkins <brian.harkins@wearecodenation.com>
2024-06-20 12:07:15 +0100

    Finished Session code

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,48 @@ import { allHeroes } from './HeroData';
 import './App.css'
 
 const App = () => {
+  const [favourites, setFavourites] = useState([]);
 
+  const handleAddToFaves = (hero) => {
+    let newFaveArr = [...favourites];
+    newFaveArr.push(hero);
+    setFavourites(newFaveArr);
+  };
 
+  const handleRemove = (index) => {
+    let newFaveArr = [...favourites];
+    newFaveArr.splice(index, 1);
+    setFavourites(newFaveArr);
+  };
 
   return (
     
       <div>
-        <h1>My Superhero Wiki</h1>
+        <h1>Superhero Wiki</h1>
 
-        {
-          allHeroes.map((heroInfo, index) => {
-            return <HeroCard key={index} heroObj={heroInfo}/>
-          })
-        }
-      </div>      
-      
+        <div>
+          <h3>My Favourite Heroes</h3>
+          {favourites.map((faveHero, index) => {
+            return <Favourites key={index} favHeroData={faveHero} removeFave={() => handleRemove(index)}/>
+          })}
+        </div>
+
+        <div>
+          <h3>All Heroes</h3>
+          {allHeroes.map((heroInfo, index) => {
+            return <HeroCard key={index} heroObj={heroInfo} faveFunc={handleAddToFaves}/>
+          })}
+        </div>
+      </div> 
+  )
+};
+
+const Favourites = ({ favHeroData, removeFave }) => {
+  return (
+    <div>      
+        <p>{favHeroData.hero}</p>
+        <button onClick={removeFave}>X</button>
+    </div>
   )
 }
 
@@ -35,7 +62,8 @@ const HeroCard = (props) => {
       </div>
       )}
 
-      <button onClick={() => setShow(!show)}>SHOW INFO</button>
+      <button onClick={() => setShow(!show)}>{show ? "HIDE INFO" : "SHOW INFO"}</button>
+      <button onClick={() => props.faveFunc(props.heroObj)}>Add to Faves</button>
     </>
   )
 }
